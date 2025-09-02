6039a57385402495a39b9991168621b979b39561
aaa7260 <62923926+aaa7260@users.noreply.github.com>
2025-09-02 22:26:06 +0800

    Update projection_functions.R

--- R/projection_functions.R
+++ R/projection_functions.R
@@ -5,8 +5,7 @@ buildReferenceFromSeurat <- function(
     pca_dims = NULL,
     harmony = 'harmony',  # 也可以是 "scvi"
     umap = "umap",
-    group = NULL
-) { 
+    group = NULL) {
   if (!assay %in% c("RNA", "SCT")) {
     log_message(
       "Only supported assays are RNA or SCT.",
